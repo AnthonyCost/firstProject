102b103bad4d4bc004a3c0ecf40d3fdb1cefe253
Machaelmus <michaeljtufo@gmail.com>
2021-04-03 18:58:56 -0400

    Search buttons have functionality

--- googleHomePage.js
+++ googleHomePage.js
@@ -1,3 +1,24 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-})
+//Whatever the user types into the search box, it will store as a cookie!
+    let input = document.getElementById('searchBar');
+    let googleButton = document.getElementById('search-button-1');
+
+        googleButton.addEventListener('click', event => {
+        let value = input.value;
+        document.cookie = `Search=${value}`;
+    });
+
+
+    let luckyButton = document.getElementById('search-button-2')
+    let luckyDiv = document.getElementById('feeling-lucky-response');
+
+    luckyButton.addEventListener('click', event => {
+        luckyDiv.innerHTML = 'Sorry! I cannot do this right now!'
+        setTimeout(function() {
+            location.reload();
+        }, 3000)
+    })
+
+
+});
