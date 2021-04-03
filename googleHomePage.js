847633713414c8aa125318fb1380f01f19087221
Machaelmus <michaeljtufo@gmail.com>
2021-04-03 19:38:13 -0400

    JS final touches and bug fixes

--- googleHomePage.js
+++ googleHomePage.js
@@ -1,15 +1,19 @@
 window.addEventListener('DOMContentLoaded', event => {
 
-//Whatever the user types into the search box, it will store as a cookie!
+//Whatever the user types into the search box, it will store that value as a cookie!
     let input = document.getElementById('searchBar');
     let googleButton = document.getElementById('search-button-1');
 
         googleButton.addEventListener('click', event => {
-        let value = input.value;
-        document.cookie = `Search=${value}`;
+           function createCookies(name, value) {
+               value = input.value;
+               document.cookie = `${name}=${value}`
+               location.reload();
+           }
+        createCookies('Search');
     });
 
-
+//When the user clicks "Im feeling lucky", it displays a message underneath the buttons and then reloads the page after a 3 second delay.
     let luckyButton = document.getElementById('search-button-2')
     let luckyDiv = document.getElementById('feeling-lucky-response');
 
